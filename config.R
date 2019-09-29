5d97edc8e881f44243631a6931cbb5ee9ae87a7d
Stephan Meylan <meylan.stephan@gmail.com>
2019-09-29 10:34:57 -0700

    initial commit for paper repo

---
+++ config.R
@@ -0,0 +1,7 @@
+paths = list()
+
+# path to original Piantadosi et al. 2011 datasets
+paths[['P11_Google1T']] = "/shared_hd0/corpora/Google1T_Piantadosi/"
+
+# Paths to currrent recomputed datasets
+paths[['MG_Google1T']] = "/home/stephan/notebooks/frequency-vs-info-content-data/"
---
+++ import_summary_tables.sh
@@ -0,0 +1,1 @@
+rsync -aP -r --exclude='2gram*.txt' --exclude='3gram*.txt' --exclude='*.LM' --exclude='*.lm' --exclude='*.zs' /shared_ssd/ss/30March_GoogleUnigram/ data/
